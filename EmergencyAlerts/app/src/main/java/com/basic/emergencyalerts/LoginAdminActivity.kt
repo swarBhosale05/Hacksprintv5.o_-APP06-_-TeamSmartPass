8f15cbf8500ef85fa1eaaa6cc0d6837d03c86618
SwaraBhosale-Dev <swar.bhosale55@gmail.com>
2024-03-02 16:29:02 +0530

    updated

--- EmergencyAlerts/app/src/main/java/com/basic/emergencyalerts/LoginAdminActivity.kt
+++ EmergencyAlerts/app/src/main/java/com/basic/emergencyalerts/LoginAdminActivity.kt
@@ -2,6 +2,7 @@ package com.basic.emergencyalerts
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.basic.emergencyalerts.databinding.ActivityLoginAdminBinding
 import com.google.firebase.database.*
@@ -26,25 +27,25 @@ class LoginAdminActivity : AppCompatActivity() {
         }
     }
 
-//    private fun loginUser(phoneNumber: String, password: String) {
-//        database.ref.child(phoneNumber).addListenerForSingleValueEvent(object : ValueEventListener {
-//            override fun onDataChange(snapshot: DataSnapshot) {
-//                if (snapshot.exists()) {
-//                    val adminData = snapshot.getValue(Admin::class.java)
-//                    if (adminData?.password == password) {
-//                        startActivity(Intent(applicationContext, AlertActivity::class.java))
-//                        finish()
-//                    } else {
-//                        Toast.makeText(applicationContext, "Incorrect password", Toast.LENGTH_SHORT).show()
-//                    }
-//                } else {
-//                    Toast.makeText(applicationContext, "User not found", Toast.LENGTH_SHORT).show()
-//                }
-//            }
-//
-//            override fun onCancelled(error: DatabaseError) {
-//                Toast.makeText(applicationContext, "Login failed", Toast.LENGTH_SHORT).show()
-//            }
-//        })
-//    }
+    private fun loginUser(phoneNumber: String, password: String) {
+        database.ref.child(phoneNumber).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    val adminData = snapshot.getValue(Admin::class.java)
+                    if (adminData?.password == password) {
+                        startActivity(Intent(applicationContext, AlertActivity::class.java))
+                        finish()
+                    } else {
+                        Toast.makeText(applicationContext, "Incorrect password", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(applicationContext, "User not found", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(applicationContext, "Login failed", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
 }
